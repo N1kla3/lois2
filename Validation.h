87d771cdd9601455bb171ca0297254e8789499d5
Nicola <kolya.vladimirsky@gmail.com>
2021-02-07 19:19:53 +0300

    validation tests

---
+++ Validation.h
@@ -0,0 +1,6 @@
+//
+// Created by nicola on 06/02/2021.
+//
+#pragma once
+
+bool Validate(const std::string& str) noexcept;
--- tests/testValidation.cpp
+++ tests/testValidation.cpp
@@ -2,8 +2,34 @@
 // Created by Nicola on 05.02.2021.
 //
 #include "gtest/gtest.h"
+#include "../Validation.h"
 
 TEST(ValidationTests, first)
 {
+    EXPECT_FALSE(Validate("asdf"));
+}
 
+TEST(ValidationTests, second)
+{
+    EXPECT_TRUE(Validate("A > B"));
+}
+
+TEST(ValidationTests, third)
+{
+    EXPECT_TRUE(Validate("((A ~ B) & C)"));
+}
+
+TEST(ValidationTests, fourth)
+{
+    EXPECT_TRUE(Validate("A | B | D | G"));
+}
+
+TEST(ValidationTests, five)
+{
+    EXPECT_FALSE(Validate("(AA & B | (C ~ B)"));
+}
+
+TEST(ValidationTests, six)
+{
+    EXPECT_TRUE(Validate("(G <D) ~(H> -O)"));
 }
