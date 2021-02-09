950e9cbefe61226b35b1e5c81e3de4b102da313e
Nicola <kolya.vladimirsky@gmail.com>
2021-02-09 21:06:01 +0300

    almost

--- Unpossible.cpp
+++ Unpossible.cpp
@@ -11,17 +11,17 @@
 constexpr char alphabet[] = {'Q', 'W', 'E','R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J',
                          'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M'};
 
-enum class OPERATIONS
-{
-    AND,
-    OR,
-    IMPLICATION_LEFT,
-    IMPLICATION_RIGHT,
-    EQUAL,
-    MAX
+const std::unordered_map<char, int> priority{
+        {'(', 7},
+        {'-', 6},
+        {'&', 5},
+        {'|', 4},
+        {'<', 3},
+        {'>', 3},
+        {'~', 1}
 };
 
-bool checkWithConstants(const std::unordered_map<char, bool>& map, const std::string& str);
+bool checkWithConstants(const std::unordered_map<char, bool>& vars, const std::string& str);
 
 bool isPossible(const std::string& str) noexcept
 {
@@ -31,7 +31,7 @@ bool isPossible(const std::string& str) noexcept
         return false;
     }
 
-    std::unordered_map<char, bool> vars;
+    std::unordered_map<char, bool> vars{{'1', true}, {'0', false}};
 
     // find variables
     for (auto lit : str)
@@ -64,7 +64,7 @@ bool isPossible(const std::string& str) noexcept
 }
 
 
-template<OPERATIONS ops>
+template<char ops>
 bool performOperation(bool first, bool second)
 {
     std::cout << "Wrong operation\n";
@@ -72,41 +72,75 @@ bool performOperation(bool first, bool second)
 }
 
 template<>
-bool performOperation<OPERATIONS::AND>(bool first, bool second)
+bool performOperation<'&'>(bool first, bool second)
 {
     return first && second;
 }
 
 template<>
-bool performOperation<OPERATIONS::OR>(bool first, bool second)
+bool performOperation<'|'>(bool first, bool second)
 {
     return first || second;
 }
 
 // ->
 template<>
-bool performOperation<OPERATIONS::IMPLICATION_LEFT>(bool first, bool second)
+bool performOperation<'>'>(bool first, bool second)
 {
     return !(first && !second);
 }
 
 // <-
 template<>
-bool performOperation<OPERATIONS::IMPLICATION_RIGHT>(bool first, bool second)
+bool performOperation<'<'>(bool first, bool second)
 {
     return !(!first && second);
 }
 
 template<>
-bool performOperation<OPERATIONS::EQUAL>(bool first, bool second)
+bool performOperation<'~'>(bool first, bool second)
 {
     return first == second;
 }
 
-bool checkWithConstants(const std::unordered_map<char, bool>& map, const std::string& str)
+bool checkWithConstants(const std::unordered_map<char, bool>& vars, const std::string& str)
 {
+    constexpr char literals[] = {'Q', 'W', 'E','R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J',
+                                'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '1', '0'};
 
     std::stack<char> operation_stack{};
     std::stack<int> values{};
+
+    auto checkVar = [&vars, &literals, &values](char literal) -> bool{
+        for (auto ch : literals)
+        {
+            if (ch == literal)
+            {
+                values.push(vars.at(ch));
+                return true;
+            }
+        }
+        return false;
+    };
+
+    int brace_count = 0;
+
+    for (auto literal : str)
+    {
+        if (literal == ' ') continue;
+        if (literal == '(') brace_count++;
+
+        if (literal == ')')
+        {
+            //TODO  close brace logic
+        }
+
+        if (!checkVar(literal))
+        {
+            operation_stack.push(literal);
+        }
+    }
+
+    //TODO line ended
 
 }
